a0e82b618852988ca8f728d29ab7ec547301136a
C0Gil <gilbertocruz2007@outlook.com>
2024-09-20 10:20:08 -0600

    Primer Modulo Odoo 16

---
+++ peliculas/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name': 'Modulo de peliculas',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Gilberto C. O.',
+    'category': 'Peliculas',
+    'description': '''
+      Modulo para hacer presupuestos de peliculas
+    ''',
+    'data': [],
+}
